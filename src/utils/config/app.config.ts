d65f7b295c5b1624bfccb5efb7e925899bf638b2
ProsperCoded <enweremproper@gmail.com>
2025-04-13 14:26:19 +0100

    🔧 Update CORS_OPTIONS to include additional origin for production environment - Added 'https://uninav-buildminds.vercel.app' to the allowed origins

--- src/utils/config/app.config.ts
+++ src/utils/config/app.config.ts
@@ -8,7 +8,7 @@ const HELMET_OPTIONS: HelmetOptions = {
 };
 
 const CORS_OPTIONS: CorsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://uninav-buildminds.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: [
     'Content-Type',
